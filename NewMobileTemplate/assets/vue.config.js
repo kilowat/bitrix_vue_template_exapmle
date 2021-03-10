982e22b8433fd2c6080b4ef008185bb74eff315e
Sergey Kadantsev <kadantsev.sergey@gmail.com>
2021-03-10 20:15:00 +0300

    Update vue.config.js
    init

--- NewMobileTemplate/assets/vue.config.js
+++ NewMobileTemplate/assets/vue.config.js
@@ -1,9 +1,17 @@
+const path = require("path");
+const templateName = path.resolve(__dirname, '..').split(path.sep).pop();
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
+    devServer: {
+        writeToDisk: true // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    },
     transpileDependencies: [
         'vuetify'
     ],
     outputDir: '../dist',
-    publicPath : '/local/templates/NewMobileTemplate/dist',
+    publicPath : `/local/templates/${templateName}/dist`,
+
     pages: {
         index: {
             // entry for the page
@@ -18,23 +26,26 @@ module.exports = {
         config.module.rule('images').use('url-loader')
             .loader('file-loader') // replaces the url-loader
             .tap(options => Object.assign(options, {
-                name: '[name].[hash:8].[ext]'
+                name: 'img/[name].[hash:8].[ext]'
             }))
         config.module.rule('svg').use('file-loader')
             .tap(options => Object.assign(options, {
-                name: '[name].[hash:8].[ext]'
+                name: 'svg/[name].[hash:8].[ext]'
             }))
     },
     css: {
         extract: {
-            filename: '[name].[hash:8].css',
-            chunkFilename: '[name].[hash:8].css'
+            filename: 'css/[name].[hash:8].css',
+            chunkFilename: 'css/[name].[hash:8].css'
         }
     },
     configureWebpack: {
+        plugins: [
+            new CleanWebpackPlugin()
+        ],
         output: {
-            filename: '[name].[hash:8].js',
-            chunkFilename: '[name].[hash:8].js'
+            filename: 'js/[name].[hash:8].js',
+            chunkFilename: 'js/[name].[hash:8].js'
         }
     }
 }
